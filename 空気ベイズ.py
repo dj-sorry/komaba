589e86371898fbcd67a65d7d4d90c532aee30b13
ye <duma@g.ecc.u-tokyo.ac.jp>
2020-10-05 20:58:32 +0300

    Update 空気ベイズ.py

--- 空気ベイズ.py
+++ 空気ベイズ.py
@@ -1,8 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
 import nagisa
 import collections
 
-text1 = "時代を漂う湿った空気が読みとれる"
-text2 = "水を含んだ湿った空気が流れてくる"
+
+text1 = "水を含んだ湿った空気が流れてくる"
+text2 = "時代を漂う湿った空気が読みとれる"
 
 doc = nagisa.tagging(text1 + text2)
 def tokenize_jp(doc):
@@ -33,9 +40,9 @@ text1_final_c1 = text1_c1_probability * percent_air1
 text1_final_c2 = text1_c2_probability * percent_air2
 
 if text1_final_c1 > text1_final_c2:
-    print("The word 空気 has the first meaning in the first text")
+    print("「空気」の意味：地球を包んでいる無色透明の気体")
 else:
-    print("The word 空気 has the second meaning in the first text")
+    print("「空気」の意味：その場の気分，雰囲気")
     
 text2_c1_probability = 1
 text2_c2_probability = 1
@@ -51,6 +58,7 @@ text2_final_c1 = text2_c1_probability * percent_air1
 text2_final_c2 = text2_c2_probability * percent_air2
 
 if text2_final_c1 > text2_final_c2:
-    print("The word 空気 has the first meaning in the first text")
+    print("「空気」の意味：地球を包んでいる無色透明の気体")
 else:
-    print("The word 空気 has the second meaning in the first text")
+    print("「空気」の意味：その場の気分，雰囲気")
+
